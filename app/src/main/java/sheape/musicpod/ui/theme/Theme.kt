d1e673a83ce455d10d064fb58a9e52e87485134e
Sheape <86521166+Sheape@users.noreply.github.com>
2025-07-07 17:01:58 +0800

    style: added catppuccin mocha colors

--- app/src/main/java/sheape/musicpod/ui/theme/Theme.kt
+++ app/src/main/java/sheape/musicpod/ui/theme/Theme.kt
@@ -3,35 +3,41 @@ package sheape.musicpod.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Red,
+    onPrimary = Text,
+    error = Red,
+    background = Base,
+    surface = Surface0,
 )
 
+// TODO: Add light theme colors.
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Red,
+    onPrimary = Text,
+    error = Red,
+    background = Base,
+    surface = Surface0,
 )
+
+@Composable
+fun extendedColor(light: Color, dark: Color): Color {
+    return if(isSystemInDarkTheme()) dark else light
+}
+
+val ColorScheme.success: Color @Composable get() = extendedColor(Green, Green)
+val ColorScheme.warning: Color @Composable get() = extendedColor(Yellow, Yellow)
+val ColorScheme.create: Color @Composable get() = extendedColor(Green, Green)
 
 @Composable
 fun MusicPodTheme(
